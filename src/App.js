c3482f953df01c274d502626d9fd4efa0544f822
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2023-07-18 14:22:09 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./globalStyles";
+import { darkTheme, lightTheme } from "./components/Themes";
+import { Route, Routes } from "react-router-dom";
+
+import Main from "./components/Main";
+import AboutPage from "./components/AboutPage";
+import BlogPage from "./components/BlogPage";
+import WorkPage from "./components/WorkPage";
+import MySkillsPage from "./components/MySkillsPage";
+
 function App() {
-  return <div>
-    App File
+  return (
+    <div>
+      <GlobalStyle />
+
+      <ThemeProvider theme={lightTheme}>
+        <Routes>
+          <Route exact path="/" Component={Main} />
+          <Route exact path="/about" Component={AboutPage} />
+          <Route exact path="/blog" Component={BlogPage} />
+          <Route exact path="/work" Component={WorkPage} />
+          <Route exact path="/skills" Component={MySkillsPage} />
+        </Routes>
+      </ThemeProvider>
     </div>
-    
+  );
 }
 
-export default App
-
+export default App;
---
+++ src/components/WorkPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WorkPage = () => {
+  return <div>WorkPage Components/Page</div>;
+};
+
+export default WorkPage;
